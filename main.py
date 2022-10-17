4af909bc9f9b700b9defef1f434db9d77aef6d53
Katie M <wintersnowwillow@gmail.com>
2022-10-17 13:19:30 -0400

    Searchbar, app uses some Graphics some Widgets

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+# from kivy.core.video import Video
 
-
-class HomePage(Widget):
-    pass
 
 class MMApp(App):
     def build(self):
+
+        # player = Video(source='sample.mp4')
+        # # player.state = 'play'
+        # # player.options = {'allow_stretch': True}
+        # return(player)
+
         root_widget = DrawingWidget()
+
+        searchbar = TextInput(text = "Search")
+        root_widget.add_widget(searchbar)
         return root_widget
 
 
@@ -21,11 +29,22 @@ class DrawingWidget(Widget):
             Color(1, 1, 1, 1)
             self.rect = Rectangle(size=self.size,
                                   pos=self.pos)
-            Color(0, 0.533, 0.412, 0.8)  # note that we must reset the colour
+            Color(0, 0.533, 0.412, 0.8)
             Rectangle(size=(Window.width, 100),
                       pos=(0, Window.height - 100))
+        
         self.bind(pos=self.update_rectangle,
                   size=self.update_rectangle)
+
+        
+        
+
+        # player = Video(filename='sample.mp4')
+        # player.autoplay = True
+        # player.state = 'play'
+        # player.options = {'allow_stretch': True}
+                
+        
 
     def update_rectangle(self, instance, value):
         self.rect.pos = self.pos
