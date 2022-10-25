0ae90354e41b20d52e2f060e0ac7268f2588a2b5
Katie M <wintersnowwillow@gmail.com>
2022-10-25 15:10:04 -0400

    Buttons in correct order

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-# from kivy.graphics import Rectangle, Color
-from kivy.core.window import Window
+from kivy.graphics import Rectangle, Color
+# from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
@@ -14,32 +14,38 @@ from kivy.uix.button import Button
 
 class MMApp(App):
     def build(self):
-
         # player = Video(source='sample.mp4')
         # # player.state = 'play'
         # # player.options = {'allow_stretch': True}
         # return(player)
 
-        main_layout = FloatLayout()
+        main_layout = FloatLayout(
+            size_hint = (1, 1),
+            pos_hint = {'x' : 0, 'y' : 0}
+        )
 
         # menu bar contains the website title (button redirecting to main page), search bar, and 3 buttons
-        menubar = BoxLayout(
-            size_hint = (.5, .25),
-            pos_hint = ({'x': .2, 'y': .2}),
-            
-            
+        menubar = FloatLayout(
+            size_hint = (1, 0.25),
+            pos_hint = ({'x': 0, 'y': 0.75}),
         )
+
+        background = DrawingWidget()
+        menubar.add_widget(background)
 
         # page title
         page_title = Label(
-            text='Multicast Menu Mobile'
+            text='Multicast Menu Mobile',
+            size_hint = (0.2, 0.2),
+            pos_hint = {'x' : 0.05, 'y' : 0.6}
             )
         menubar.add_widget(page_title)
 
         # search bar
         searchbar = TextInput(
             text = 'Search',
-            size_hint = (.6, .25)
+            size_hint = (0.3, 0.25),
+            pos_hint = {'x' : 0.3, 'y' : 0.6}
             )
         menubar.add_widget(searchbar)
 
@@ -58,43 +64,60 @@ class MMApp(App):
 
         # Login button
         login_button = Button(
-            text = 'Log in'
+            text = 'Log in',
+            size_hint = (0.1, 0.2),
+            pos_hint = {'x' : 0.7, 'y' : 0.6}
         )
         menubar.add_widget(login_button)
 
         # Register button
         register_button = Button(
-            text = 'Register'
+            text = 'Register',
+            size_hint = (0.1, 0.2),
+            pos_hint = {'x' : 0.8, 'y' : 0.6}
         )
         menubar.add_widget(register_button)
+        main_layout.add_widget(menubar)
 
-
-        main_layout.add_widget(menubar,'top')
+        # main_layout.add_widget(menubar,'top')
         
+        # placeholder_stream = Button(
+        #     size_hint = (0.2, 0.2),
+        #     pos_hint = {'x' : 0.5, 'y' : 0.5}
+        # )
+        # main_layout.add_widget(placeholder_stream)
 
-
-        
-        # layout.add_widget(DrawingWidget())
-
+    
         # searchbar = TextInput(text = 'Search')
         # layout.add_widget(searchbar)
         return main_layout
 
 
+# extends the Widget class to have a custom canvas
 class DrawingWidget(Widget):
     def __init__(self):
         super(DrawingWidget, self).__init__()
 
+        self.size_hint = (1, 1)
+
+        # for whatever reason, this is allowing me to put the rectangle outside of where the drawing class actually is.
         with self.canvas:
-            Color(1, 1, 1, 1)
-            self.rect = Rectangle(size=self.size,
-                                  pos=self.pos)
-            Color(0, 0.533, 0.412, 0.8)
-            Rectangle(size=(Window.width, 100),
-                      pos=(0, Window.height - 100))
+
+            # Color(1, 1, 1, 1)
+            # self.rect = Rectangle(
+            # )
+            Color(0, 0.533, 0.412, 1)
+            self.rect = Rectangle(pos = (self.width / 2., self.height / 2.),
+                                  size =(self.width / 2.,
+                                        self.height / 2.))
         
-        self.bind(pos=self.update_rectangle,
-                  size=self.update_rectangle)
+        self.bind(pos = self.update_rect,
+                  size = self.update_rect)
+
+    # update function which makes the canvas adjustable. FROM GEEKS FOR GEEKS - rework this and the self.rect part above
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
         
         
