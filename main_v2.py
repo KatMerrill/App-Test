14388403df77b749f64821eda042e3a7c10e80ee
Katie M <wintersnowwillow@gmail.com>
2023-02-23 15:59:20 -0500

    test files for reference

--- main_v2.py
+++ main_v2.py
@@ -4,18 +4,17 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
+from kivy.clock import mainthread
+
 
 # ScreenManager keeps track of all Screens
 class WindowManager(ScreenManager):
     pass
 
-class WatchScreen(Screen):
-    pass
-
-class HomeScreen(Screen):
-    pass
+placeholder_streams = []
 
 Window.clearcolor = (255, 255, 255, 255)
+Window.maximize()
 # used to load a file other than the default, which is my.kv
 kv = Builder.load_file("myApp.kv")
 
@@ -23,8 +22,9 @@ STREAM_COUNT = 3
 
 # all classes referred to in the .kv files don't need any code here. Note that this inherits from Screen
 class HomeScreen(Screen):
+    
+    @mainthread
     def on_enter(self):
-        placeholder_streams = []
         for num in range(STREAM_COUNT):
             # note that these are placeholders, but real streams will be stored in some sort of list similar to this
             placeholder_streams.append(
@@ -37,13 +37,14 @@ class HomeScreen(Screen):
             )
             self.ids.scrolling_layout.add_widget(placeholder_streams[num])
 
-
+class WatchScreen(Screen):
+    pass
 
 # the app only has to build itself, since everything else is contained in the .kv files
 class MyMainApp(App):
-    def build(self):
-        Window.maximize()
-        return kv
+    pass
+    # def build(self):
+    #     return kv
 
 if __name__ == "__main__":
     MyMainApp().run()
