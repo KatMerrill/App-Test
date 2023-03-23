25a3480443a9d4e2001233cb156d0fecfb4df438
Bradley Cao <bradleycao@gmail.com>
2023-03-23 15:37:59 -0400

    opencv + ffmpeg stream demo (to localhost)

--- send-pylivestream.py
+++ send-pylivestream.py
@@ -1,5 +1,135 @@
+import threading
 import subprocess
-from pynput.keyboard import Key, Controller
-keyboard = Controller()
+from functools import partial
+import numpy as np
+import cv2
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-subprocess.call(['python', '-m', 'pylivestream.camera', 'localhost', './pylivestream.json', '-y'])
+class MainScreen(Screen):
+    pass
+
+class Manager(ScreenManager):
+    pass
+
+        
+def open_ffmpeg_stream_process():
+    args = (
+        "ffmpeg -re -stream_loop -1 -f rawvideo -s 1920x1080 -i pipe:0 -pix_fmt yuv420p -f mpegts udp://127.0.0.1:1234"
+    ).split()
+    return subprocess.Popen(args, stdin=subprocess.PIPE)
+
+def capture_loop():
+    ffmpeg_process = open_ffmpeg_stream_process()
+    capture = cv2.VideoCapture()    
+    while True:
+        grabbed, frame = capture.read()
+        if not grabbed:
+            break
+        ffmpeg_process.stdin.write(frame.astype(np.uint8).tobytes())
+    capture.release()
+    ffmpeg_process.stdin.close()
+    ffmpeg_process.wait()
+
+Builder.load_string('''
+<MainScreen>:
+    name: "Test"
+
+    FloatLayout:
+        Label:
+            text: "Webcam from OpenCV?"
+            pos_hint: {"x":0.0, "y":0.8}
+            size_hint: 1.0, 0.2
+
+        Image:
+            id: vid
+            size_hint: 1, 0.6
+            allow_stretch: True  # allow the video image to be scaled
+            keep_ratio: True  # keep the aspect ratio so people don't look squashed
+            pos_hint: {'center_x':0.5, 'top':0.8}
+
+        Button:
+            text: 'Stop Video'
+            pos_hint: {"x":0.0, "y":0.0}
+            size_hint: 1.0, 0.2
+            font_size: 50
+            on_release: app.stop_vid()
+''')
+
+
+class Main(App):
+    def build(self):
+
+        # start the camera access code on a separate thread
+        # if this was done on the main thread, GUI would stop
+        # daemon=True means kill this thread when app stops
+        threading.Thread(target=self.doit, daemon=True).start()
+
+        sm = ScreenManager()
+        self.main_screen = MainScreen()
+        sm.add_widget(self.main_screen)
+        return sm
+
+    def doit(self):
+        # this code is run in a separate thread
+        self.do_vid = True  # flag to stop loop
+
+        # make a window for use by cv2
+        # flags allow resizing without regard to aspect ratio
+        cv2.namedWindow('Hidden', cv2.WINDOW_NORMAL | cv2.WINDOW_FREERATIO)
+
+        # resize the window to (0,0) to make it invisible
+        cv2.resizeWindow('Hidden', 0, 0)
+        cam = cv2.VideoCapture(0)
+
+        # start processing loop
+        streamProcess = open_ffmpeg_stream_process()
+        while (self.do_vid):
+            print("Capturing frame")
+            ret, frame = cam.read()
+            if not ret:
+                break
+
+            # send this frame to the kivy Image Widget
+            # Must use Clock.schedule_once to get this bit of code
+            # to run back on the main thread (required for GUI operations)
+            # the partial function just says to call the specified method with the provided argument (Clock adds a time argument)
+            Clock.schedule_once(partial(self.display_frame, frame))
+            streamProcess.stdin.write(frame.astype(np.uint8).tobytes())        ### BROKEN PIPE?
+
+            cv2.imshow('Hidden', frame)
+            cv2.waitKey(1)
+        cam.release()
+        streamProcess.stdin.close()
+        streamProcess.wait()
+        cv2.destroyAllWindows()
+
+    def stop_vid(self):
+        # stop the video capture loop
+        self.do_vid = False
+
+    def display_frame(self, frame, dt):
+        # display the current video frame in the kivy Image widget
+
+        # create a Texture the correct size and format for the frame
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+
+        # copy the frame data into the texture
+        texture.blit_buffer(frame.tobytes(order=None), colorfmt='bgr', bufferfmt='ubyte')
+
+        # flip the texture (otherwise the video is upside down
+        texture.flip_vertical()
+
+        # actually put the texture in the kivy Image widget
+        self.main_screen.ids.vid.texture = texture
+
+
+if __name__ == '__main__':
+    Main().run()
+
+### SOURCES:
+# https://stackoverflow.com/questions/69379674/how-to-stream-cv2-videowriter-frames-to-and-rtsp-server
+# https://stackoverflow.com/questions/61615277/is-there-a-way-to-integrate-the-imshow-function-of-opencv-into-kivy-or-kv-file
